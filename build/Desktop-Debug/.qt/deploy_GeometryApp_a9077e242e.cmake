210e7fa75123d9a75435a834dd8a894fa07382a4
Nikita Nikitov <nikitanikitov@MacBook-Air-Nikita-2.local>
2026-04-18 13:18:57 +0200

    gitignore

--- build/Desktop-Debug/.qt/deploy_GeometryApp_a9077e242e.cmake
+++
@@ -1,7 +0,0 @@
-include("/Users/nikitanikitov/GeometryApp/build/Desktop-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/GeometryApp-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "GeometryApp.app"
-)
